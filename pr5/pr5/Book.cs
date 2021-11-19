b954817d7ed7b764089f37f649067d2720b78fa5
Екатерина Копылова <ek53084@gmail.com>
2021-11-19 12:43:20 +0900

    Пр

---
+++ pr5/pr5/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace pr5
+{
+    class Book
+    {
+        public int idbooks { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public string Author { get; set; }
+        public int DateCreate { get; set; }
+    }
+}
